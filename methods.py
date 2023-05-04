d8e9eeac5cac8412d9c184c9765861927cde061f
GuiKastro01 <114615817+GuiKastro01@users.noreply.github.com>
2023-05-04 09:25:06 -0300

    Update methods.py

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum_of_values(valor1, valor2):
+    sum = valor1 + valor2
+    return sum
+
+def sub_of_values(valor1, valor2):
+    sub = valor1 - valor2
+    return sub
+
+def mult_of_values(valor1, valor2):
+    mult = valor1 * valor2
+    return mult
+
+def div_of_values(valor1, valor2):
+    div = valor1 / valor2
+    return div
